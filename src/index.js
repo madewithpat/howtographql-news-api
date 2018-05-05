f1fd4e429ffb29aca3244e3bd3d07b488e111046
Patrick Outler <outler.patrick@gmail.com>
2018-05-05 17:54:12 -0400

    Step Five
    More prisma setup; add generated directory to gitignore

--- src/index.js
+++ src/index.js
@@ -1,43 +1,38 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
-
-let idCount = links.length;
+const { Prisma } = require("prisma-binding");
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
-  },
-
-  Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info);
     }
   },
 
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url
+  Mutation: {
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
+          }
+        },
+        info
+      );
+    }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: req => ({
+    typeDefs: "src/generated/prisma.graphql",
+    endpoint: "https://eu1.prisma.sh/public-leafduke-410/hackernews-node/dev",
+    secret: "supersecret",
+    debug: true
+  })
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
